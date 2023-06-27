c6242011d01872965ca967caefd73d547c9e872b
Taylor Salo <tsalo006@fiu.edu>
2023-06-27 15:27:54 -0400

    Run XCP-D on the reduced fMRIPrep data.

---
+++ run_xcpd_fmriprep.sh
@@ -0,0 +1,23 @@
+docker run --rm -u $(id -u) \
+    -v /Users/taylor/Documents/linc/xcp_d_test_data/data:/bids-input:rw \
+    -v /Users/taylor/Documents/tsalo/xcp_d/xcp_d:/usr/local/miniconda/lib/python3.8/site-packages/xcp_d \
+    -v /Users/taylor/Documents/tsalo/xcp_d_testing/data/license.txt:/license.txt \
+    --env FS_LICENSE=/license.txt \
+    pennlinc/xcp_d:unstable \
+    /bids-input/fmriprep-reduced \
+    /bids-input/derivatives \
+    participant \
+    --input-type fmriprep \
+    -w /bids-input/work \
+    --participant_label 1648798153 \
+    --dummy-scans auto \
+    --head_radius auto \
+    --warp-surfaces-native2std \
+    --cifti \
+    --motion-filter-type notch \
+    --band-stop-min 12 \
+    --band-stop-max 18 \
+    --mem_gb 4 \
+    --nthreads 1 \
+    --resource_monitor \
+    -v
