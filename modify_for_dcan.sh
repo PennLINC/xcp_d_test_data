bbcd37e0ace06b1fa8c0863c1a316737118a0257
Taylor Salo <tsalo006@fiu.edu>
2023-07-14 17:34:29 -0400

    Update call.
    It's failing on the warp though.

--- modify_for_dcan.sh
+++ modify_for_dcan.sh
@@ -1,4 +1,7 @@
 docker run --rm -ti \
     -v /Users/taylor/Documents/linc/xcp_d_test_data:/Users/taylor/Documents/linc/xcp_d_test_data \
-    --entrypoint "python /Users/taylor/Documents/linc/xcp_d_test_data/modify_for_dcan.py" \
-    pennlinc/xcp_d:unstable
+    -v /Users/taylor/Documents/tsalo/xcp_d_testing/data/license.txt:/license.txt \
+    -e FS_LICENSE=/license.txt \
+    --entrypoint python \
+    pennlinc/xcp_d:unstable \
+    /Users/taylor/Documents/linc/xcp_d_test_data/modify_for_dcan.py
