830b926c132e35b9c1ef67a18e459b23b8a09f70
Taylor Salo <tsalo006@fiu.edu>
2023-07-26 16:40:02 -0400

    Run it again.

--- modify_for_dcan.sh
+++ modify_for_dcan.sh
@@ -30,6 +30,7 @@ done
 docker run --rm -ti \
     -v /Users/taylor/Documents/linc/xcp_d_test_data:/Users/taylor/Documents/linc/xcp_d_test_data \
     -v /Users/taylor/Documents/tsalo/xcp_d_testing/data/license.txt:/license.txt \
+    -v /Users/taylor/Documents/tsalo/xcp_d/xcp_d:/usr/local/miniconda/lib/python3.8/site-packages/xcp_d \
     -e FS_LICENSE=/license.txt \
     --entrypoint python \
     pennlinc/xcp_d:unstable \
