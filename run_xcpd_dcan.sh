0af8ffcfd392b71fd98ee93660fb3012b46b2ae7
Taylor Salo <tsalo006@fiu.edu>
2023-07-27 11:29:53 -0400

    Create run_xcpd_dcan.sh

---
+++ run_xcpd_dcan.sh
@@ -0,0 +1,22 @@
+docker run --rm -u $(id -u) \
+    -v /Users/taylor/Documents/linc/xcp_d_test_data/data:/bids-input:rw \
+    -v /Users/taylor/Documents/tsalo/xcp_d/xcp_d:/usr/local/miniconda/lib/python3.8/site-packages/xcp_d \
+    -v /Users/taylor/Documents/tsalo/xcp_d_testing/data/license.txt:/license.txt \
+    --env FS_LICENSE=/license.txt \
+    pennlinc/xcp_d:unstable \
+    /bids-input/derivatives/dcan \
+    /bids-input/derivatives \
+    participant \
+    --input-type dcan \
+    -w /bids-input/work \
+    --participant_label 1648798153 \
+    --dummy-scans auto \
+    --head-radius auto \
+    --warp-surfaces-native2std \
+    --cifti \
+    --motion-filter-type notch \
+    --band-stop-min 12 \
+    --band-stop-max 18 \
+    --mem-gb 4 \
+    --nthreads 1 \
+    -v
